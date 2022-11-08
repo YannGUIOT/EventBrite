ef1187a6833b8d5730a82ff78309af659a57a696
YannGUIOT <guiot_yann@hotmail.fr>
2022-11-08 17:32:50 +0100

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
+
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
@@ -8,5 +13,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
- root "static_pages#index"
+ root "events#index"
 end
